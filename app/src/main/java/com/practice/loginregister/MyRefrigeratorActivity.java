159334193df638d0f48db32c6f084f8cdc98319f
kksshh0612 <kksshh0612@gmail.com>
2022-06-12 20:31:23 +0900

    세부동작 잘 안됨

---
+++ app/src/main/java/com/practice/loginregister/MyRefrigeratorActivity.java
@@ -0,0 +1,106 @@
+package com.practice.loginregister;
+//나의 냉장고 동작
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MyRefrigeratorActivity extends Activity {
+
+    private EditText ref_name;  //현재 로그인한 사용자 이름 담을 Edittext
+    private Button refAdd;
+    private Button refDel;
+    private ListView refList;
+    private String name;        //현재 로그인한 사용자 이름 담을 변수
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myrefrigerator);
+
+        //SQLite 연결 관련 변수들
+        SQLiteDatabase db;
+        DatabaseHelper db_helper;       //디비 동작 관련 함수 있는 객체
+        db_helper = new DatabaseHelper(this);
+        db = db_helper.getWritableDatabase();               //OnCreate 생성 안됨
+
+        Intent getintent = getIntent();
+        name = getintent.getStringExtra("name");        //현재 로그인한 사용자 이름 저장
+
+        ref_name = findViewById(R.id.ref_name);
+        ref_name.setText(name + "의 냉장고");
+
+        //재료추가 버튼 이동
+        refAdd = findViewById(R.id.ref_add);
+
+        refAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Add();
+            }
+        });
+
+
+        //DB에 있는 추가한 재료 string으로 가져와서 listview에 배열로 넣기
+        String[][] my_refrigerator_arr = new String[20][2];        //재료와 유통기한 넣을 2차원 배열
+        my_refrigerator_arr =  db_helper.MyRefrigerator(db, name);
+        List<String> data = new ArrayList<>();
+
+        for(int i=0; i<my_refrigerator_arr.length; i++){            //행의 수만큼 반복
+            data.add(my_refrigerator_arr[i][0] + "/" + my_refrigerator_arr[i][1]);      // 재료/소비기한 형식으로 데이터 추가가
+       }
+
+        refList = (ListView) findViewById(R.id.ref_listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, data);   //리스트뷰와 연결할 어댑터
+        refList.setAdapter(adapter); //adapter를 list에 set. >> ListView와 Adapter 연결
+
+        Button refDel = (Button) findViewById(R.id.ref_del);
+
+        refDel.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int count, checked ;
+                count = adapter.getCount() ;
+
+                if (count > 0) {
+                    // 현재 선택된 아이템의 position 획득.
+                    checked = refList.getCheckedItemPosition();
+
+                    if (checked > -1 && checked < count) {
+                        // 아이템 삭제
+                        data.remove(checked) ;
+
+                        // listview 선택 초기화.
+                        refList.clearChoices();
+
+                        // listview 갱신.
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+
+            }
+        });
+    }
+
+    //재료추가 버튼 이동
+    public void Add() {
+        Intent intent = new Intent(this, IngredientSelectActivity.class);
+        startActivity(intent);
+    }
+
+}
+
+
+
+
